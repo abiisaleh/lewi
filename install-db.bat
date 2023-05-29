6ebdc845ad198327a1f35200ecc90c44b846b9a9
abiisaleh <abi.saleh.25@gmail.com>
2023-05-29 10:16:38 +0900

    fix bat

--- install-db.bat
+++ install-db.bat
@@ -10,4 +10,3 @@ php spark auth:set_password siswa 1234demo
 php spark auth:create_group admin 'kelola data master'
 php spark auth:create_group guru 'kelola data siswa per kelas'
 php spark auth:create_group siswa 'kelola data pribadi'
-start run.bat
