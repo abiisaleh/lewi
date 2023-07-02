42fc472d0f3c3de0b46e16a0d23472c86d6b83e0
abiisaleh <abi.saleh.25@gmail.com>
2023-07-02 10:06:49 +0900

    update navbar

--- app/Views/user/_navbar.php
+++ app/Views/user/_navbar.php
@@ -8,50 +8,18 @@
                 </a>
             </li>
 
-            <li class="menu-item  ">
+            <li class="menu-item">
                 <a href="#cari" class='menu-link'>
                     <i class="bi bi-search"></i>
                     <span>Cari Siswa</span>
                 </a>
             </li>
 
-            <li class="menu-item  has-sub">
+            <li class="menu-item">
                 <a href="#profile" class='menu-link'>
                     <i class="bi bi-stack"></i>
                     <span>Profile</span>
                 </a>
-                <div class="submenu ">
-                    <!-- Wrap to submenu-group-wrapper if you want 3-level submenu. Otherwise remove it. -->
-                    <div class="submenu-group-wrapper">
-
-                        <ul class="submenu-group">
-
-                            <li class="submenu-item  ">
-                                <a href="component-alert.html" class='submenu-link'>Sejarah</a>
-
-
-                            </li>
-
-
-
-                            <li class="submenu-item  ">
-                                <a href="component-badge.html" class='submenu-link'>Jurusan</a>
-
-
-                            </li>
-
-
-
-                            <li class="submenu-item  ">
-                                <a href="component-breadcrumb.html" class='submenu-link'>Fasilitas</a>
-
-
-                            </li>
-
-                        </ul>
-
-                    </div>
-                </div>
             </li>
 
             <li class="menu-item">
